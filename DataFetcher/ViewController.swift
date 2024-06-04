46a9e6374d58095480c536b803f5d475bdfd3b82
Gustaf Gunér <gustaf@guner.se>
2024-06-04 23:18:15 +0200

    Initial Commit

---
+++ DataFetcher/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataFetcher
+//
+//  Created by Gustaf Gunér on 2024-06-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
